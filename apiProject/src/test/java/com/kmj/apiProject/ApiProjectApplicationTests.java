a4b1c29287b0aa738ab17d3f2fde47bd2fd00893
강무진 <@192.168.0.8>
2025-03-18 01:59:41 +0900

    최초커밋

---
+++ apiProject/src/test/java/com/kmj/apiProject/ApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmj.apiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
