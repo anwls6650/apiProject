50dfea5bf73728f22af6f81d8ac3bad858b1e13c
강무진 <anwls6650@gmail.com>
2025-04-09 18:18:38 +0900

    fix : 기사 위치 test에서 자동 큐이름 생성

--- src/main/java/com/kmj/apiProject/common/service/DriverLocationProducer.java
+++ src/main/java/com/kmj/apiProject/common/service/DriverLocationProducer.java
@@ -2,6 +2,10 @@ package com.kmj.apiProject.common.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kmj.apiProject.auth.dto.DriverDto;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,15 +18,20 @@ public class DriverLocationProducer {
 
     @Autowired
     private ObjectMapper objectMapper;
+    
+    @Autowired
+    private AmqpAdmin amqpAdmin;
 
     // 메시지 전송 메소드
     public void sendMessage(DriverDto driverDto) {
         try {
             // DTO를 JSON 문자열로 변환
             String message = objectMapper.writeValueAsString(driverDto);
-
+            String queueName = "driver_queue_" + driverDto.getDriverId();
+            Queue queue = new Queue(queueName, true);
+            amqpAdmin.declareQueue(queue);
             // RabbitMQ에 메시지 전송
-            rabbitTemplate.convertAndSend("driver_queue", message);  // "driver_queue"는 RabbitMQ 큐 이름
+            rabbitTemplate.convertAndSend(queueName, message);  // "driver_queue"는 RabbitMQ 큐 이름
         } catch (Exception e) {
             e.printStackTrace();
         }
